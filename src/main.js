c5a0fb5fec927c375dddb24936754057cf4b3da8
Igor <diduniki@gmail.com>
2019-04-16 20:59:28 +0300

    all before banner without slaider

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import"./reset.css";
-import"./style.scss";
-import"./grid.scss";
-import"./index.js"
+import "./reset.css";
+import "./style.scss";
+import "./grid.scss";
+import "./index.js";
+import "slick";
 
-
+$('.your-class').slick({
+    setting-name: setting-value
+  });
+ 
