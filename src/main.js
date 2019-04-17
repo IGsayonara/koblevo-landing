10d784be06ede6bb7b9a00701532eb46314b8336
Igor <diduniki@gmail.com>
2019-04-17 20:50:21 +0300

    new: banner and feedback

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import "./reset.css";
 import "./style.scss";
 import "./grid.scss";
-import "./index.js";
-import "slick";
+import "./slick.css";
 import "jquery"
 
-$(document).ready(function(){
-    $('.slider').slick();
-});
