dcca4d888477cbda3c03fb91f8918c3d0c98d408
Igor <diduniki@gmail.com>
2019-04-18 20:45:22 +0300

    new: slaider(1 problem)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import "./reset.css";
 import "./style.scss";
 import "./grid.scss";
-import "./slick.css";
-import "jquery"
+require ("slick-carousel/slick/slick")
+import "slick-carousel/slick/slick.scss"
+
+console.log($.slick);
+
+$( document ).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: false,
+        infinite: true,
+        autoplaySpeed: 5000,
+    });
+});
 
