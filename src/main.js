7e326b3244c3376c4459dadfb1bad4fb4f84edb8
Mykola Kovrizhnykh <intendantoy@gmail.com>
2019-04-26 14:13:44 +0300

    Upd.

--- src/main.js
+++ src/main.js
@@ -250,7 +250,7 @@ $(document).ready(function () {
                 console.log( response );
                 $(this).parent().append(`
                     <div class="text-center p-2">
-                        ${!response.error ? '<img src="./ico/success.png" alt="">':''}
+                        ${!response.error ? '<img src="./src/ico/success.png" alt="">':''}
                         <h2>${response.heading}</h2>
                         <p>${response.message}</p>
                     </div>
